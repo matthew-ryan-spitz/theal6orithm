ab3ad38b71df4d8ffee772b373ad2b07d6555aaa
matthew spitz <mrspitz6@gmail.com>
2024-04-09 20:16:34 +0000

    added season param to get_tourney_round_names macro

--- march_madness_24/macros/categorical_transformations.sql
+++ march_madness_24/macros/categorical_transformations.sql
@@ -1,15 +1,15 @@
 {# returns round name of game played in nccaa tournament;
     results will be incorrect in 2021 season due to scheduling change #}
 
-{% macro get_tourney_round_names(day_num) -%}
+{% macro get_tourney_round_names(day_num, season) -%}
 
-    case when {{day_num}} = 134 or {{day_num}} = 135 then 'play_in'
-        when {{day_num}} = 136 or {{day_num}} = 137 then 'round_1'
-        when {{day_num}} = 138 or {{day_num}} = 139 then 'round_2'
-        when {{day_num}} = 143 or {{day_num}} = 144 then 'sweet_sixteen'
-        when {{day_num}} = 145 or {{day_num}} = 146 then 'elite_eight'
-        when {{day_num}} = 152 then 'final_four'
-        when {{day_num}} = 154 then 'championship'
+    case when {{season}} != 2021 and {{day_num}} = 134 or {{day_num}} = 135 then 'play_in'
+        when {{season}} != 2021 and {{day_num}} = 136 or {{day_num}} = 137 then 'round_1'
+        when {{season}} != 2021 and {{day_num}} = 138 or {{day_num}} = 139 then 'round_2'
+        when {{season}} != 2021 and {{day_num}} = 143 or {{day_num}} = 144 then 'sweet_sixteen'
+        when {{season}} != 2021 and {{day_num}} = 145 or {{day_num}} = 146 then 'elite_eight'
+        when {{season}} != 2021 and {{day_num}} = 152 then 'final_four'
+        when {{season}} != 2021 and {{day_num}} = 154 then 'championship'
         else null
     end
 
