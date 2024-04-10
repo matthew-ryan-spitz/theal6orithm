0697ba3d0edd8b8e315a0b67f48796bea6bf3783
matthew spitz <mrspitz6@gmail.com>
2024-04-10 22:06:02 +0000

    changed team rankings mart to daily table

--- march_madness_24/models/core/mart_daily_team_rankings_agg.sql
+++ march_madness_24/models/core/mart_daily_team_rankings_agg.sql
@@ -4,9 +4,9 @@
     )
 }}
 
-with output as (
+with team_rankings as (
     select r.season,
-        r.ranking_day_num,
+        r.games_calculated_day_num,
         r.team_id,
         t.team_name,
         round(avg(r.ordinal_rank),0) as ordinal_rank_system_avg,
@@ -16,6 +16,14 @@ with output as (
     left join {{ ref('stg_teams') }} as t
         on r.team_id = t.team_id
     group by 1,2,3,4
+),
+
+output as (
+    select *
+    from team_rankings as tr
+    cross join unnest(generate_array(0, {{ var('max_game_day_num') }} )) as day_num
+    where tr.games_calculated_day_num < day_num
+    qualify row_number() over(partition by season, day_num, team_id, team_name order by games_calculated_day_num desc) = 1
 )
 
 select * from output
