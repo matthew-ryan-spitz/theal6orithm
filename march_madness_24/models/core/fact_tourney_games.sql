2076cb9f89d3a58e12290fdc9ac936db503e1551
matthew spitz <mrspitz6@gmail.com>
2024-04-11 14:59:26 +0000

    updated tourney game results fact table

--- march_madness_24/models/core/fact_tourney_games.sql
+++ march_madness_24/models/core/fact_tourney_games.sql
@@ -4,15 +4,10 @@
     )
 }}
 
-
--- join season
--- join team_ids to get team names and seeds
--- join seeds to get slots and matchups
--- join team_rankings
 with output as (
     select tr.game_id,
         tr.season,
-        s.day_zero + tr.day_num as game_date,
+        date(s.day_zero) + tr.day_num as game_date,
         tr.day_num,
         tr.tourney_round_name,
         srs.game_round as tourney_round_number,
@@ -34,33 +29,40 @@ with output as (
         tr.winning_team_score,
         tr.losing_team_score,
         tr.winning_team_location,
-        case when tr.winning_team_location = 'home' then 1 else 0 end as home_win_flag
+        case when tr.winning_team_location = 'home' then 1 else 0 end as home_win_flag,
+        case when trk1.ordinal_rank_system_avg is null and trk2.ordinal_rank_system_avg is null then null
+            when coalesce(trk1.ordinal_rank_system_avg, 1000000) < coalesce(trk2.ordinal_rank_system_avg, 1000000) then 1 else 0 end as favorite_rank_win_flag,
+        case when seed < sd2.seed then 1 else 0 end as favorite_seed_win_flag
     from {{ ref('stg_tourney_results_compact') }} as tr
+    -- join team_ids
     left join {{ ref('stg_teams') }} as t1
-        on tr.winning_team_id = t.team_id
+        on tr.winning_team_id = t1.team_id
     left join {{ ref('stg_teams') }} as t2
-        on tr.losing_team_id = t.team_id
+        on tr.losing_team_id = t2.team_id
+    -- join season
     left join {{ ref('stg_seasons') }} as s
         on tr.season = s.season
+    -- join seeds to get slots and matchups
     left join {{ ref('stg_tourney_seeds') }} as sd1
         on tr.season = sd1.season
-            and tr.winning_team_id.team_id = sd1.team_id
+            and tr.winning_team_id = sd1.team_id
     left join {{ ref('stg_tourney_seeds') }} as sd2
         on tr.season = sd2.season
-            and tr.losing_team_id.team_id = sd2.team_id
+            and tr.losing_team_id = sd2.team_id
     left join {{ ref('stg_tourney_seed_round_slots') }} as srs
         on sd1.seed = srs.seed
             and tr.day_num between srs.early_day_num and srs.late_day_num
     left join {{ ref('stg_tourney_slots') }} as ss
         on tr.season = ss.season
             and srs.game_slot = ss.slot
+    -- join team_rankings
     left join {{ ref('mart_daily_team_rankings_agg')}} as trk1
         on tr.season = trk1.season
             and tr.winning_team_id = trk1.team_id
             and tr.day_num = trk1.day_num
     left join {{ ref('mart_daily_team_rankings_agg')}} as trk2
         on tr.season = trk2.season
-            and tr.winning_team_id = trk2.team_id
+            and tr.losing_team_id = trk2.team_id
             and tr.day_num = trk2.day_num
 )
 
