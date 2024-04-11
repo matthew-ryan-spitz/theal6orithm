9b45ff7ba209a63a9e4996fd88341e7089a41dba
matthew spitz <mrspitz6@gmail.com>
2024-04-11 14:15:39 +0000

    updated regular season results fact table

--- march_madness_24/models/core/fact_regular_season_games.sql
+++ march_madness_24/models/core/fact_regular_season_games.sql
@@ -4,13 +4,10 @@
     )
 }}
 
--- join season
--- join team_ids
--- join team_rankings
 with output as (
     select tr.game_id,
         tr.season,
-        s.day_zero + tr.day_num as game_date,
+        date(s.day_zero) + tr.day_num as game_date,
         tr.day_num,
         tr.winning_team_id,
         t1.team_name as winning_team_name,
@@ -22,20 +19,23 @@ with output as (
         tr.losing_team_score,
         tr.winning_team_location,
         case when tr.winning_team_location = 'home' then 1 else 0 end as home_win_flag
-    from {{ ref('stg_tourney_results_compact') }} as tr
+    from {{ ref('stg_regular_season_results_compact') }} as tr
+    -- join team_ids
     left join {{ ref('stg_teams') }} as t1
-        on tr.winning_team_id = t.team_id
+        on tr.winning_team_id = t1.team_id
     left join {{ ref('stg_teams') }} as t2
-        on tr.losing_team_id = t.team_id
+        on tr.losing_team_id = t2.team_id
+    -- join season
     left join {{ ref('stg_seasons') }} as s
         on tr.season = s.season
+    -- join team_rankings
     left join {{ ref('mart_daily_team_rankings_agg')}} as trk1
         on tr.season = trk1.season
             and tr.winning_team_id = trk1.team_id
             and tr.day_num = trk1.day_num
     left join {{ ref('mart_daily_team_rankings_agg')}} as trk2
         on tr.season = trk2.season
-            and tr.winning_team_id = trk2.team_id
+            and tr.losing_team_id = trk2.team_id
             and tr.day_num = trk2.day_num
 )
 
